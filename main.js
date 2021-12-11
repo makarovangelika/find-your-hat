6ba1e23bb67a4bcc90f72140600a6d4d02188f62
Angelika Makarova <esipovaangelika@mail.ru>
2021-12-11 16:47:45 +0300

    Change the method of choosing the field for hat and for the start of the game. Create character for player position to distinguish from path character

--- main.js
+++ main.js
@@ -4,14 +4,32 @@ const term = require( 'terminal-kit' ).terminal;
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
+const character = 'p';
 const pathCharacter = '*';
 
 class Field {
   constructor(field) {
     this.field = field;
-    this.locationX = Math.floor(Math.random() * this.field[0].length);
-    this.locationY = Math.floor(Math.random() * this.field.length);
-    this.field[this.locationY][this.locationX] = pathCharacter;
+    const freeFields = [];
+    this.field.forEach((row, y) => {
+      row.forEach((cell, x) => {
+        if (cell === fieldCharacter) {
+          freeFields.push({x: x, y: y})
+        }
+      });
+    });
+    if (freeFields.length >= 2) {
+      freeFields.sort((a, b) => 0.5 - Math.random());
+    } else {
+      throw 'Not enough free fields';
+    }
+
+    this.player = freeFields.pop();
+    this.playerPrev = {x: this.player.x, y: this.player.y}
+    this.field[this.player.y][this.player.x] = character;
+
+    this.hat = freeFields.pop();
+    this.field[this.hat.y][this.hat.x] = hat;
   }
   print() {
     function arrayToString(item) {
@@ -22,18 +40,20 @@ class Field {
   }
   askQuestion() {
     const direction = prompt('Which way?');
+    this.playerPrev.x = this.player.x
+    this.playerPrev.y = this.player.y
     switch (direction) {
       case 'r':
-        this.locationX += 1;
+        this.player.x += 1;
         break;
       case 'l':
-        this.locationX -= 1;
+        this.player.x -= 1;
         break;
       case 'd':
-        this.locationY += 1;
+        this.player.y += 1;
         break;
       case 'u':
-        this.locationY -= 1;
+        this.player.y -= 1;
         break;
       default:
         term.red('Enter r, l, u or d');
@@ -42,17 +62,17 @@ class Field {
     }
   }
   isHat() {
-    return this.field[this.locationY][this.locationX] === hat;
+    return this.field[this.player.y][this.player.x] === hat;
   }
   isHole() {
-    return this.field[this.locationY][this.locationX] === hole;
+    return this.field[this.player.y][this.player.x] === hole;
   }
   isOutOfBounds() {
     return (
-    this.locationY < 0 ||
-    this.locationY >= this.field.length ||
-    this.locationX < 0 ||
-    this.locationX >= this.field[0].length);
+    this.player.y < 0 ||
+    this.player.y >= this.field.length ||
+    this.player.x < 0 ||
+    this.player.x >= this.field[0].length);
     }
   runGame() {
     this.print();
@@ -64,9 +84,17 @@ class Field {
     } else if (this.isHat()) {
       term.yellow('Congrats, you found your hat!');
     } else {
-      this.field[this.locationY][this.locationX] = pathCharacter;
+      this.field[this.playerPrev.y][this.playerPrev.x] = pathCharacter;
+      this.field[this.player.y][this.player.x] = character;
       this.runGame();
     }
+  }
+  solver() {
+    /*const hatUp = this.field[hatRow - 1][hatIndex];
+    const hatDown = this.field[hatRow + 1][hatIndex];
+    const hatLeft = this.field[hatRow][hatIndex - 1];
+    const hatRight = this.field[hatRow][hatIndex + 1];*/
+    
   }
   static generateField(height, width, percentage = 0.1) {
     const field = new Array(height).fill(0).map(el => new Array(width));
@@ -76,17 +104,18 @@ class Field {
         field[y][x] = random > percentage ? fieldCharacter : hole;
       }
     }
-    const hatLocation = {
-      y: Math.floor(Math.random() * height),
-      x: Math.floor(Math.random() * width)
-    }
-    while (hatLocation.y === this.locationY && hatLocation.x === this.locationX) {
-      hatLocation.y = Math.floor(Math.random() * height);
-      hatLocation.x = Math.floor(Math.random() * width);
-    }
-    field[hatLocation.y][hatLocation.x] = hat;
     return field;
   }
 }
-const myField = new Field(Field.generateField(7, 7, 0.2));
+
+const example = [
+  ['O', 'O', '░', 'O'],
+  ['░', '░', '░', '░'],
+  ['O', '░', '░', 'O'],
+  ['O', '░', 'O', '░'],
+  ['░', '░', 'O', 'O'],
+  ['░', 'O', 'O', '░'],
+]
+const myField = new Field(example);
+
 myField.runGame();
